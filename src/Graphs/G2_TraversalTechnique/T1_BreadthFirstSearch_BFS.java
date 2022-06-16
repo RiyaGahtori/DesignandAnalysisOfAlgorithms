44698db9f3f23a7428af25ff88da81f6d933d82d
RiyaGahtori <riyagahtori21@gmail.com>
2022-06-16 15:30:07 +0530

    Commit

--- src/Graphs/G2_TraversalTechnique/T1_BreadthFirstSearch_BFS.java
+++ src/Graphs/G2_TraversalTechnique/T1_BreadthFirstSearch_BFS.java
@@ -1,4 +1,39 @@
 package Graphs.G2_TraversalTechnique;
 
-public class T1_BreadthFirstSearch_BFS {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class T1_BreadthFirstSearch_BFS
+{
+    public ArrayList<Integer> G2_AdjacencyList(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        ArrayList<Integer>bfs = new ArrayList<>();
+        boolean vis[] = new boolean[V+1];
+
+        for(int i=1; i<=V; i++)
+        {
+            if(vis[i] == false)
+            {
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                vis[i] = true;
+
+                while(!q.isEmpty())
+                {
+                    Integer node = q.poll();
+                    bfs.add(node);
+                    for(Integer it: adj.get(node))
+                    {
+                        if(vis[it] == false)
+                        {
+                            vis[it] = true;
+                            q.add(it);
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
